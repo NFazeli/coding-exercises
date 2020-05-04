fe911e25d30d2b84d16be9f28bf6c449917f2048
NFazeli <45542036+NFazeli@users.noreply.github.com>
2020-05-04 13:06:13 +0200

    add new exercise

---
+++ fibonacci/Factorial.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    """
+    >>> factorial(0)
+    0
+    >>> factorial(1)
+    1
+    >>> factorial(2)
+    2
+    """
+
+    if n in [0, 1]:
+        return 1
+    else:
+        return n
