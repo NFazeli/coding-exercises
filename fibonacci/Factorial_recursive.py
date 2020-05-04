fb2bc6f90299f9e04206af42200a33486b6df97b
NFazeli <45542036+NFazeli@users.noreply.github.com>
2020-05-04 13:21:39 +0200

    add new exercise

--- fibonacci/Factorial_recursive.py
+++ fibonacci/Factorial_recursive.py
@@ -1,14 +1,20 @@
 def factorial(n):
     """
     >>> factorial(0)
-    0
+    1
     >>> factorial(1)
     1
     >>> factorial(2)
     2
+    >>> factorial(3)
+    6
+    >>> factorial(5)
+    120
+    >>> factorial(4)
+    24
     """
 
+    assert n >= 0
     if n in [0, 1]:
         return 1
-    else:
-        return n
+    return n*factorial(n-1)
